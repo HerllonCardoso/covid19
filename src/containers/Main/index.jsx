6368a647e1f275d47291f3f3dc23ea39e5317b19
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-08-23 14:48:15 -0300

    pwa init

---
+++ src/containers/Main/index.jsx
@@ -0,0 +1,48 @@
+import React, { memo, useState, useCallback, useEffect } from 'react';
+import Api from '../../api';
+import { ContainerStyled } from './styles';
+import Panel from './components/Panel';
+import Board from './components/Board';
+
+
+function Main(){
+    const [data, setData] = useState({})
+    const [country, setCountry] = useState('brazil')
+    const updateAt = new Date().toLocaleString()
+
+    const getCovidData = useCallback((country) => {
+        Api.getCountry(country)
+            .then(data => setData(data))
+    }, []);
+
+    useEffect(() => {
+        getCovidData(country)
+    }, [getCovidData, country])
+
+    const handleChange = ({ target }) => {
+        const country = target.value
+        setCountry(country)
+    }
+
+    return(
+        <div>
+        <ContainerStyled>
+            <div className="mb-2">
+                <Panel 
+                    data={data}
+                    updateAt={updateAt}
+                    onChange={handleChange}
+                    country={country}
+                    getCovidData={getCovidData}
+                
+                />
+
+            </div>
+            <Board data={data} />
+
+        </ContainerStyled>
+        </div>
+    )
+}
+
+export default memo(Main);
