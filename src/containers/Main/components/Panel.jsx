8cea7a8fc32778928c029f0f994c331c0b1c0699
Herllon Cardoso <cardoso.herllon@gmail.com>
2020-08-23 15:55:15 -0300

    fix some issues

--- src/containers/Main/components/Panel.jsx
+++ src/containers/Main/components/Panel.jsx
@@ -53,8 +53,10 @@ function Panel({ updateAt, onChange, data, country, getCovidData }) {
           <CardPanelContentStyled>
             <div>
               <Typography variant="h5" component="span" color="primary">COVID19</Typography>
-              <Typography variant="h6" component="span" color="primary">Painel Coronavírus</Typography>
-              <Typography variant="body2" component="span" color="primary">Atualizado em: {updateAt}</Typography>
+              <br/>
+              <Typography variant="h6" component="span">Painel Coronavírus</Typography>
+              <br/>
+              <Typography variant="body2" component="span" color="secondary">Atualizado em: {updateAt}</Typography>
               <img src={RefreshIcon} alt="Atualizar" onClick={() => getCovidData(country)} className="cursor" />
               <div className="pt-2">
                 <Select onChange={onChange} value={country}>
